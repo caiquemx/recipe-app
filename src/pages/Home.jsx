e3b28ea1460644683ec3ed7a8e0df5c0734fac96
Caique Moreira <caique_moreira_sz@hotmail.com>
2023-01-31 18:42:54 -0300

    feat: Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <section className="categories-container"></section>
+      <section className="meal-container"></section>
+    </div>
+  );
+}
